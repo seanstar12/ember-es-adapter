a1d926778f62efde1c5f2dbf1bbe12ae1e989c5b
Sean Starnes <seanstar12@gmail.com>
2016-10-04 02:23:06 -0500

    added ember

--- tests/unit/serializers/elasticsearch-test.js
+++ tests/unit/serializers/elasticsearch-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('elasticsearch', 'Unit | Serializer | elasticsearch', {
   // Specify the other units that are required for this test.
