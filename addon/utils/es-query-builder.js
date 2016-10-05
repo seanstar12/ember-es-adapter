71e8094cb6475597b6cba558b7e7989d50827a3f
Sean Starnes <seanstar12@gmail.com>
2016-10-04 20:06:51 -0500

    added Elasticsearch query builder utility

---
+++ addon/utils/es-query-builder.js
@@ -0,0 +1,256 @@
+import extend from 'ember-es-adapter/utils/extend';
+
+const defaultQuerySize = 10;
+/**
+* Builds queries for ElasticSearch.
+* Usage: 
+*   ```javascript
+*   let es, myQuery;
+*   es = new esQuery();
+*   es.addBoolMatchField('title', 'ECO');
+*   myQuery = es.getQuery();
+*   ```
+*
+* @class esQueryBuilder
+* @constructor
+*/
+let esQueryBuilder = function(opts) {
+  opts = opts ? opts : {};
+
+  this.sort = [];
+  this.options = this._options(opts);
+  this.query = this._templateBoolQuery();
+  this._defaultSortSet = false;
+};
+
+/**
+* Private: 
+* Sets the options variable for the class.
+*
+* @method _options
+* @private
+* @param {Object} options Overrides for the class.
+* @return {Object} default = { 'sortType': 'desc', 'sort': '_score' };
+*/
+esQueryBuilder.prototype._options = function(options) {
+  options = options ? options : {};
+
+  let defaults = {
+    'from': 0,
+    'size': defaultQuerySize
+  };
+
+  return extend(defaults, options);
+};
+
+/**
+* Public: 
+* Returns options from the class.
+*
+* @method getOptions
+* @return {Object} { 'sortType': 'desc', 'sort': '_score' }
+*/
+esQueryBuilder.prototype.getOptions = function() {
+  return this.options;
+};
+
+/**
+* Public: 
+* Gets the query in it's current state.
+*
+* @method getQuery
+* @return {Object} Object state of query. 
+*/
+esQueryBuilder.prototype.getQuery = function() {
+  return this.query;
+};
+
+/**
+* Private: 
+* Generates the template for a "Bool" query.
+*   ```json
+*   {
+*     "query": {
+*       "bool": {
+*         "must": [],  
+*         "filter": [],  
+*         "must_not": [],  
+*         "should": [],  
+*       }
+*     }
+*   }
+*   ```
+*
+* @method _templateBoolQuery
+* @private
+* @return {Object} Returns the default structure for the query.
+*/
+esQueryBuilder.prototype._templateBoolQuery = function() {
+  return {
+     "query": {
+       "bool": {
+         "must": [],  
+         "filter": [],  
+         "must_not": [],  
+         "should": [],  
+       }
+     }
+  };
+};
+
+/**
+* Adds a sort param. Takes a string or object. These are stackable
+* so multiple sorts can be applied.
+* 
+*   ```json
+*   Example use:
+*   let es, myQuery;
+*   es = new esQuery();
+*   es.addSort('title');
+*   es.addSort({'date' : { 'order': 'asc'}});
+*   es.getQuery(); 
+*   ```
+*
+*   ```json
+*   Example Sorts
+*   `simple sort`
+*   "user"
+*
+*   `less simple sort`
+*   { "name": "asc" }
+*
+*   `complexish simple sort`
+*   { "post_date": { "order": "asc"} }
+*
+* @method addSort
+* @param {Object} sort Sort object/string to be added.
+*/
+esQueryBuilder.prototype.addSort = function(sort) {
+  this.sort.push(sort);
+};
+
+/**
+* Adds a sort param.
+*
+* @method _addDefaultSort
+* @private
+*/
+esQueryBuilder.prototype._addDefaultSort = function() {
+  let options = this.options;
+  let sort;
+
+  if (options.sort) {
+    if (options.sortType) {
+      let defaultSort = {};
+      defaultSort[options.sort] = options.sortType;
+      sort = defaultSort;
+    }
+    else {
+      sort = options.sort;
+    }
+
+    this._defaultSortSet = true;
+    this.addSort(sort);
+  }
+};
+
+/**
+* Public: 
+* Adds a query to Bool. Allows for complex queries.
+*
+*   ```json
+*   Example objects:
+* 
+*   `complex match`
+*   {
+*     "match": {
+*       "message": {
+*         "query": "to be or not to be",
+*         "operator": "and",
+*         "zero_terms_query": "all"
+*       }
+*     }
+*   }
+* 
+*   `simple match`
+*   {
+*     "match": {
+*       "message": "this is a test"
+*     }
+*   }
+* 
+*   `other queries`
+*   {
+*     "term": {
+*       "tag": "tech"
+*     }
+*   }
+*   ```
+*
+* @method addBool
+* @param {Object} query Query parameters
+* @param {String} type Type of Bool query should be applied to. 
+*                 [must,filter,must_not,should]. Defaults to 'must'.
+*/
+esQueryBuilder.prototype.addBool = function(obj, type) {
+  type = type ? type : 'must';
+  
+  this._addBool(obj, type);
+};
+
+/**
+* Private: 
+* Adds a query to Bool. Secrely.
+*
+* @method _addBool
+* @private
+* @param {Object} query Query parameters
+* @param {String} type Type of Bool query should be applied to. 
+*                 [must,filter,must_not,should]. Defaults to 'must'.
+*/
+esQueryBuilder.prototype._addBool = function(obj, type) {
+  if (this.query.query.bool[type]) {
+    if (typeof obj === 'object') {
+      this.query.query.bool[type].push(obj);
+    }
+  }
+};
+
+/**
+* Public: 
+* Adds a query to Bool with the type 'match'.
+*   ```json
+*   {
+*     "match": {
+*       "{field}": "{value}"
+*     }
+*   }
+*   ```
+*
+* @method addBoolMatchField
+* @param {String} field Field name to run query against.
+* @param {String} value Value of field.
+*/
+esQueryBuilder.prototype.addBoolMatchField = function(fieldName, value) {
+  let bool = {
+    "match": {}
+  };
+
+  bool.match[fieldName] = value;
+
+  this._addBool(bool, 'must');
+};
+
+esQueryBuilder.prototype.buildQuery = function() {
+  if (!this._defaultSortSet) {
+    this._addDefaultSort();
+  }
+
+  let sort = this.sort;
+  let query = this.query;
+  let { from, size } = this.options;
+
+  return { query, sort, from, size };
+};
+
+export default esQueryBuilder;
---
+++ tests/unit/utils/es-query-builder-test.js
@@ -0,0 +1,94 @@
+import EsQuery from 'dummy/utils/es-query-builder';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | esQueryBuilder');
+
+// Replace this with your real tests.
+test('It imports', function(assert) {
+  let result = new EsQuery();
+  assert.ok(result);
+});
+
+test('Constructor constructs with properties', function(assert) {
+  let obj = { 'from' : 10 };
+  let es = new EsQuery(obj);
+  
+  assert.equal(es.options.page, obj.page, "From number is the same.");
+});
+
+test('Constructor default sort', function(assert) {
+  assert.expect(2);
+
+  let obj = { 'sort' : 'title' };
+  let es = new EsQuery(obj);
+  let query = es.buildQuery();
+  
+  assert.deepEqual(query.sort, ['title'], "Adds default sort properly.");
+
+  obj = { 'sort' : 'title', 'sortType' : 'asc' };
+  es = new EsQuery(obj);
+  query = es.buildQuery();
+
+  assert.deepEqual(query.sort, [{'title':'asc'}], "Adds default with sort-order properly.");
+});
+
+test('Adds correct sort params', function(assert) {
+  let sorts = [
+    'title',
+    { 'date' : { 'order' : 'asc' } },
+    { 'name' : 'desc' }
+  ];
+
+  let es = new EsQuery();
+
+  es.addSort('title');
+  es.addSort({ 'date' : { 'order' : 'asc' } });
+  es.addSort({ 'name' : 'desc' });
+  
+  assert.deepEqual(es.sort, sorts, "Query sort matches test array.");
+});
+
+test('Builds Complex Query with sort and constructor', function(assert) {
+  let expected = {
+    "query": {
+      "query":{
+        "bool":{
+          "must":[
+            {"match": {"title":"Im a title"} },
+            {"match":{
+              "message":{
+                "query":"to be or not to be",
+                "operator":"and",
+                "zero_terms_query":"all"
+            }}}
+          ],
+          "filter":[],"must_not":[],"should":[]
+      }}
+    },
+    "sort":[{"name":"desc"},{"title":"asc"}],
+    "from":2,
+    "size":17
+  };
+  let cons = {
+    from: 2,
+    size: 17,
+    sort: 'title',
+    sortType: 'asc'
+  };
+
+  let es = new EsQuery(cons);
+  es.addSort({ 'name' : 'desc' });
+  es.addBoolMatchField('title', 'Im a title');
+  es.addBool({
+    'match': {
+      'message': {
+        'query': 'to be or not to be',
+        'operator': 'and',
+        'zero_terms_query': 'all'
+      }
+    }
+  });
+
+  let query = es.buildQuery();
+  assert.deepEqual(query, expected, "Query was built properly");
+});
