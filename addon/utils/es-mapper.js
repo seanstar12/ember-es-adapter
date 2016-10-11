8bf0a799ae908d2810a7b4e0032e29f232d54f43
Sean Starnes <seanstar12@gmail.com>
2016-10-10 19:13:27 -0500

    Adding es-mapping util to generate models from elasticsearch mappings -- not complete [skip ci]

---
+++ addon/utils/es-mapper.js
@@ -0,0 +1,78 @@
+import config from 'ember-get-config';
+import fetch from 'ember-network/fetch';
+
+/**
+* Queries ElasticSearch to get mappings.
+*
+* @class EsMapper
+* @constructor
+*/
+class EsMapper {
+
+  constructor () {
+    this.host = config.EsAdapter.host;
+    this.namespace = config.EsAdapter.namespace;
+  }
+
+  /**
+  * Gets the mapping from ElasticSearch in form of array.
+  *
+  * @method getMapping
+  * @private
+  * @return {Object} Object containing arrays of mappings. 
+  * e.g. {'post':[{mapping:'body', type:'string'}]}
+  */
+  getMapping() {
+    let host = this.host,
+        namespace = this.namespace;
+
+    if (!this.host || !this.namespace) {
+      console.log('[EsMapper][getMapping]: Host or Namespace not defined.');
+      return false;
+    }
+
+    return new Promise(function(resolve, reject) {
+      fetch(host + '/'+ namespace + '/_mappings')
+        .then((resp) => {
+          let r = resp.json(),
+              types = {};
+
+          r.then((result) => {
+              for (var i in result) {
+                if (result[i]['mappings']) {
+                  let _types = result[i]['mappings'];
+
+                  for (var j in _types) {
+                    if (_types[j]) {
+                      let _properties = _types[j]['properties'];
+                      let _propKeys = Object.keys(_properties);
+
+                      types[j] = [];
+
+                      for (var k=0; k < _propKeys.length; k++) {
+                        types[j].push({
+                          'mapping': _propKeys[k],
+                          'type': _properties[_propKeys[k]].type
+                        }); 
+                      }
+
+                      resolve(types);
+                    }
+                    else {
+                      reject('[EsMapper][getMapping]: No Types Found.');
+                    }
+                  }
+                }
+                else {
+                  reject('[EsMapper][getMapping]: No Mappings Found.');
+                }
+              }
+            });
+
+          });
+
+    });
+  }
+}
+
+export default EsMapper;
---
+++ tests/unit/utils/es-mapper-test.js
@@ -0,0 +1,10 @@
+import esMapper from 'dummy/utils/es-mapper';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | es mapper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = esMapper();
+  assert.ok(result);
+});
