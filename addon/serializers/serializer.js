86c27895e1bdf221326a109d4e93d7cef372c6ed
Sean <seanstar12@gmail.com>
2017-08-07 09:22:28 -0500

    Updated serializer to handle embedded responses.

--- addon/serializers/serializer.js
+++ addon/serializers/serializer.js
@@ -1,10 +1,9 @@
 import DS from 'ember-data';
-
+import Ember from 'ember';
 /**
  * Handle ElasticSearch (de)serialization.
  *
  * This serializer adjusts payload data so that it is consumable by
- * ElasticSearch.
  *
  * @class EsSerializer
  * @extends DS.JSONAPISerializer
@@ -13,9 +12,13 @@ export default DS.JSONAPISerializer.extend({
   // Don't need to convert primaryKey because we're doing that with
   // normalize[X]Response
   //primaryKey: '_id',
+  isNewSerializerAPI: true,
+  attrs: {
+    masterImage: { embedded: 'always'}
+  },
 
-  serialize(/*snapshot, options*/) {
-    //console.log('[es-adapter][serializer]:[serialize]');
+  serialize: function(snapshot, options) {
+    //Ember.Logger.log('%c [es-adapter][serializer]:[serialize]', "color:white; background:brown", {snapshot, options});
     let json = this._super(...arguments),
         item = json.data,
         data = {},
@@ -40,10 +43,11 @@ export default DS.JSONAPISerializer.extend({
    * @return {Object} JSON-API Document
    */
   normalizeResponse: function(store, primaryModelClass, payload, id, requestType) {
-    //console.log('[es-adapter][serializer]:[normalizeResponse]');
-    //console.log(payload);
+    //Ember.Logger.log('%c [es-adapter][serializer]:[normalizeResponse]', 'color:white; background:green', {payload});
+    payload = {data: payload};
 
     return this._super(store, primaryModelClass, payload, id, requestType);
+    //return payload;
   },
 
   normalizeFindAllResponse: function(store, primaryModelClass, payload, id, requestType) {
@@ -59,60 +63,196 @@ export default DS.JSONAPISerializer.extend({
         };
       }),
       meta: {
-        shards: payload._shards,
+/*        shards: payload._shards,
         timed_out: payload.timed_out,
-        took: payload.took,
-        total: hits.total
+        took: payload.took,*/
+        total: hits.total,
       }
     };
-	  
+
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
 
   normalizeQueryResponse: function(store, primaryModelClass, payload, id, requestType) {
-    //console.log('[es-adapter][serializer]:[normalizeQueryResponse]');
-    let hits = payload.hits;
+    let hits = payload.data.hits;
 
+    //payload = this.extractStuff(store, primaryModelClass, payload.data.hits.hits);
     payload = {
-      data: hits.hits.map(function(hit) {
-        return {
-          id: hit._id,
-          type: hit._type,
-          attributes: hit._source
-        };
-      }),
+      data: payload.data.hits.hits,
       meta: {
-        shards: payload._shards,
+        /*shards: payload._shards,
         timed_out: payload.timed_out,
-        took: payload.took,
-        total: hits.total
+        took: payload.took,*/
+        total: hits.total,
       }
     };
 
-    return this._super(store, primaryModelClass, payload, id, requestType);
+    payload = this._super(store, primaryModelClass, payload, id, requestType);
+
+    //Ember.Logger.log('%c [es-adapter][serializer]:[normalizeQueryResponse]', 'color:white; background: #ff00c2', payload);
+
+    // Extract relationships
+
+    return payload;
   },
 
   normalizeSingleResponse: function(store, primaryModelClass, payload, id, requestType) {
-    //console.log('[es-adapter][serializer]:[normalizeSingleResponse]');
+    const serializer = store.serializerFor(primaryModelClass.modelName);
+    const primaryKey = serializer.get('primaryKey');
 
     payload = {
       data: {
-        id: payload._id,
-        type: payload._type,
-        attributes: payload._source
+        attributes: payload.data._source,
+        type: primaryModelClass.modelName,
       },
-      meta: {
-        index: payload._index,
-        found: payload.found,
-        version: payload._version
-      }
     };
 
-    return this._super(store, primaryModelClass, payload, id, requestType);
-  }
+    payload.data[primaryKey] = id;
 
-  //normalizeCreateRecordResponse(store, primaryModelClass, payload, id, requestType) {
-  //  //console.log('[es-adapter][serializer]:[normalizeSingleCreateRecordResponse]');
-  //	return this.normalizeSaveResponse(...arguments);
-  //}
+    //payload = this.extractStuff(store, primaryModelClass, [payload.data._source]);
+
+    //Ember.Logger.log('%c [es-adapter][serializer]:[normalizeSingleResponse]', 'color:white; background:#33A5FF', {payload});
+    //return payload;
+    payload = this._super(store, primaryModelClass, payload, id, requestType);
+    payload = this.extractIncluded(store, payload);
+    return payload;
+  },
+
+  extractIncluded: function(store, payload) {
+    let included = [];
+
+    if (Ember.typeOf(payload.data) === 'object') {
+      if (!payload.data.relationships) return payload;
+
+      included = [payload.data].reduce(this._extractIncluded.bind(this), included);
+    }
+    else if (Ember.typeOf(payload.data) === 'array') {
+      included = payload.data.reduce(this._extractIncluded.bind(this), included);
+
+    }
+
+
+    payload.included = included.map((item) => {
+      const serializer = store.serializerFor(item.type);
+      const primaryKey = serializer.get('primaryKey');
+      const newItem = {
+        attributes: item,
+        type: item.type,
+      };
+      newItem[primaryKey] = item[primaryKey];
+
+      const normalized = store.normalize(item.type, item);
+      normalized.data[primaryKey] = item[primaryKey]; // Force primaryKey
+      return normalized.data;
+    });
+
+    return payload;
+  },
+
+  _extractIncluded: function(previousValue, dataItem) {
+    const keys = this._getObjectKeys(dataItem.relationships);
+    const { included } = keys.reduce(
+                           this._extractIncludedReduce.bind(this),
+                           {
+                             relationships: dataItem.relationships,
+                             included: []
+                           }
+                         );
+
+    return previousValue.concat(included);
+  },
+
+  _extractIncludedReduce: function(previousValue, key) {
+    const model = previousValue.relationships[key];
+    let objs;
+
+    if (Ember.typeOf(model.data) === 'object') objs = [model.data];
+    if (Ember.typeOf(model.data) === 'array') objs = model.data;
+
+    previousValue.included = previousValue.included.concat(objs.filter(this._shouldIncludeFilter.bind(this)));
+    return previousValue;
+  },
+
+  _shouldIncludeFilter: function(item) {
+    const numKeys = this._getObjectKeys(item).length;
+
+    return (numKeys > 2); // Do more keys than id and type exist?
+  },
+
+  // @TODO does ember do this already?
+  _getObjectKeys: function(object) {
+    let objKeys = []
+    for (let key in object) {
+      if (object.hasOwnProperty(key)){
+        objKeys.push(key);
+      }
+    }
+    return objKeys;
+  },
+
+  normalizeCreateRecordResponse(store, primaryModelClass, payload, id, requestType) {
+    //console.log('[es-adapter][serializer]:[normalizeSingleCreateRecordResponse]');
+    return this.normalizeSaveResponse(...arguments);
+  },
+
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    //console.log('[es-adapter][serializer]:[normalizeArrayResponse]');
+
+    //Ember.Logger.log({primaryModelClass, payload, id, requestType});
+    payload.data = payload.data.map((item) => {
+      const serializer = store.serializerFor(item._source.type);
+      const primaryKey = serializer.get('primaryKey');
+
+      const newItem = {
+        attributes: item._source,
+        type: item._source.type
+      }
+      newItem[primaryKey] = item._source[primaryKey];
+      return newItem;
+    });
+
+    let normalizedDocument = this._super(...arguments);
+
+    normalizedDocument = this.extractIncluded(store, normalizedDocument);
+
+    return normalizedDocument;
+  },
+
+  extractRelationships: function(modelClass, resourceHash) {
+    let relationships = {};
+
+    if (!resourceHash.attributes) return relationships;
+
+    modelClass.eachRelationship((key, relationshipMeta) => {
+      let relationshipKey = this.keyForRelationship(key, relationshipMeta.kind, 'deserialize');
+      if (resourceHash.attributes.hasOwnProperty(relationshipKey)) {
+
+        let relationshipHash = {data: resourceHash.attributes[relationshipKey]};
+        if(Ember.typeOf(relationshipHash.data) === 'object') {
+          relationshipHash.data.type = relationshipMeta.type;
+        }
+        else if(Ember.typeOf(relationshipHash.data) === 'array') {
+          relationshipHash.data = relationshipHash.data.map((val) => {
+            if (typeof val === "object") {
+              val.type = relationshipMeta.type;
+              if (relationshipMeta.type === "image") {
+                val.id = val.fid;
+              }
+              return val;
+            }
+
+            // String or number
+            return {id: val, type: relationshipMeta.type};
+          });
+        }
+        else { // String or number
+          relationshipHash.data = {id: relationshipHash.data, type: relationshipMeta.type};
+        }
+
+        relationships[key] = relationshipHash;
+      }
+    });
+
+    return relationships;
+  },
 });
